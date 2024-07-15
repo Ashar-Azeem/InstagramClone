6c3e92fc0691dc90bacd710f4c25104fab5e783c
Ashar-Azeem <ashar.azeem2001@gmail.com>
2024-07-15 06:27:47 +0500

    fixed graddle

--- lib/services/CRUD.dart
+++ lib/services/CRUD.dart
@@ -4,7 +4,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/foundation.dart';
-import 'package:mysocialmediaapp/services/SendingNotification.dart';
 import 'package:mysocialmediaapp/utilities/utilities.dart';
 
 class DataBase {
